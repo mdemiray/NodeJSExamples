833ebce4e4b8a4ad68ba549010b56f3ac03d9dd0
Muharrem Demiray <muharrem.demiray@gmail.com>
2020-02-21 17:22:53 -0500

    Add files via upload

---
+++ FirstFile.js
@@ -0,0 +1,14 @@
+let variable = "Hello World!!!!!!"
+
+console.log(variable)
+
+console.log(2+2)
+
+
+console.log(__dirname)
+console.log(__filename)
+
+
+const path = require("path")
+
+console.log(`The file name is ${path.basename(__filename)}`)
